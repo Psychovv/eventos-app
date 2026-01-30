7f95722bf7aee0326bfe53904a665a6771a2627b
Psychovv <viniciusmedina63@gmail.com>
2026-01-29 22:24:24 -0300

    Add AsyncStorage support for event caching in getEvents function

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -1,9 +1,23 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { api } from "./api";
 import { Event, UpdateEventDTO, CreateEventDTO } from "../types/types";
 
+const STORAGE_KEY = "@eventos_app:lista_eventos";
+
 export async function getEvents(): Promise<Event[]> {
-  const response = await api.get<Event[]>("/events");
-  return response.data;
+  try {
+    const response = await api.get<Event[]>("/events");
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(response.data));
+    return response.data;
+  } catch (error) {
+    const cachedData = await AsyncStorage.getItem(STORAGE_KEY);
+    
+    if (cachedData) {
+      return JSON.parse(cachedData);
+    }
+    
+    throw error;
+  }
 }
 
 export async function createEvent(data: CreateEventDTO): Promise<Event> {
